ccff72e3b3518813dbaf0954615dcf469570c6d2
Kostyantyn Bilyk <kos.bilyk@gmail.com>
2016-05-07 10:40:02 +0300

    Initial Commit

---
+++ NOTAM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NOTAM
+//
+//  Created by Kostyantyn Bilyk on 07.05.16.
+//  Copyright © 2016 Kostyantyn Bilyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NOTAM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NOTAM
+//
+//  Created by Kostyantyn Bilyk on 07.05.16.
+//  Copyright © 2016 Kostyantyn Bilyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
